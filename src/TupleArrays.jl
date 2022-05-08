d6788f55f9a55536223d2ac2c389e0762a888188
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-05-08 10:23:53 -0400

    modified:   src/TupleArrays.jl

--- src/TupleArrays.jl
+++ src/TupleArrays.jl
@@ -3,41 +3,55 @@ module TupleArrays
 export AbstractTupleArray, TupleArray, AbstractNamedTupleArray, NamedTupleArray
 export TupleMatrix, TupleVector, AbstractTupleVector, AbstractTupleMatrix,
     NamedTupleVector, NamedTupleMatrix
-export gettupfields, gettupindex, tuplength, getnames
+export getfieldtup, gettupindex, tuplength, getnames
+
+# temporary export
+export _striptype, ensurevectorfields, hastuplefield
+
+###
+### AbstractTupleArray
+###
 
 abstract type AbstractTupleArray{Ttup, Nd, Nt} <: AbstractArray{Ttup, Nd} end
 const AbstractTupleVector{Ttup} = AbstractTupleArray{Ttup, 1, Nt} where {Ttup, Nt}
 const AbstractTupleMatrix{Ttup} = AbstractTupleArray{Ttup, 2, Nt} where {Ttup, Nt}
 
+# Worked around this by converting AbstractTupleArray with tuple data
+# to one with vector data when calling `show`. So we don't need the
+# piracy below.
 # piracy here. There is some bug in printing that makes
 # this necessary. Doing this:
 # show(io, MIME"text/plain"(), TupleArray((1,2),(3,4))
 # attempts getindex with two indices instead of one. Don't know why or how
-Base.getindex(tup::Tuple, i::Integer, j::Integer) = getindex(tup, i)
+#Base.getindex(tup::Tuple, i::Integer, j::Integer) = getindex(tup, i)
 
 # Length  of tuple (typically number of fields in struct)
 tuplength(ta::AbstractTupleArray{<:Any,<:Any,Nt}) where Nt = Nt
-gettupfields(ta::AbstractTupleArray) = getfield(ta, :data)
+getfieldtup(ta::AbstractTupleArray) = getfield(ta, :_data)
 
-function _gettupindex(ta::AbstractTupleArray, i::Integer)
-    return ((x[i] for x in gettupfields(ta))...,)
+"""
+    ensurevectorfields(fieldtup)
+
+Copy the tuple of containers `fieldtup` converting any `Tuple` to a `Vector`.
+This is probably not robust. Hard to say what needs to be converted.
+"""
+ensurevectorfields(fieldtup) = ((isa(x,Tuple) ? collect(x) : x for x in fieldtup)...,)
+
+ensurevectorfields(ta::AbstractTupleArray) = !hastuplefield(ta) ? ta :
+    _striptype(typeof(ta))(ensurevectorfields(getfieldtup(ta))...,)
+
+function Base.show(io::IO, ::MIME"text/plain", ta::AbstractTupleArray)
+    tav = ensurevectorfields(ta)
+    invoke(show, Tuple{IO,MIME"text/plain",AbstractVector}, io, MIME"text/plain"(), tav)
 end
 
-function _gettupindex(ta::AbstractTupleArray, i::Integer...)
-#    @show i
-    return ((x[i...] for x in gettupfields(ta))...,)
-end
-
-
-#gettupindex(args...) = _gettupindex(args...) # can collapse again
-function gettupindex(args...)
-    _gettupindex(args...)
-end
+_gettupindex(ta::AbstractTupleArray, i::Integer...) = ((x[i...] for x in getfieldtup(ta))...,)
+gettupindex(args...) = _gettupindex(args...)
 
 _size(args...) = Base.size(args...)
 _size(t::Tuple) = (length(t),) # Preclude using Tuples for higher dim arrays
-Base.size(ta::AbstractTupleArray) = _size(first(gettupfields(ta)))
-Base.length(ta::AbstractTupleArray) = length(first(gettupfields(ta)))
+Base.size(ta::AbstractTupleArray) = _size(first(getfieldtup(ta)))
+Base.length(ta::AbstractTupleArray) = length(first(getfieldtup(ta)))
 Base.getindex(ta::AbstractTupleArray, i::Integer...) = gettupindex(ta, i...)
 
 function _construct(arrays)
@@ -50,7 +64,7 @@ function _construct(arrays)
 end
 
 struct TupleArray{Ttup, Nd, Nt} <: AbstractTupleArray{Ttup, Nd, Nt}
-    data::Ttup
+    _data::Ttup
     function TupleArray(arrays...)
         return new{_construct(arrays)...}(arrays)
     end
@@ -63,15 +77,20 @@ abstract type AbstractNamedTupleArray{Ttup, Nd, Nt, Tnames} <: AbstractTupleArra
 
 getnames(::AbstractNamedTupleArray{<:Any,<:Any,<:Any,Tnames}) where Tnames = Tnames
 
+hastuplefield(ta::AbstractTupleArray) = any(x->isa(x,Tuple), getfieldtup(ta))
+
+function ensurevectorfields(ta::AbstractNamedTupleArray)
+    return _striptype(typeof(ta))(getnames(ta), ensurevectorfields(getfieldtup(ta))...,)
+end
+
 function gettupindex(ta::AbstractNamedTupleArray, i::Integer...)
-#    @show i
     tup = _gettupindex(ta, i...)
     names = getnames(ta)
     return NamedTuple{names, typeof(tup)}(tup)
 end
 
 struct NamedTupleArray{Ttup, Nd, Nt, Tnames} <: AbstractNamedTupleArray{Ttup, Nd, Nt, Tnames}
-    data::Ttup
+    _data::Ttup
     function NamedTupleArray(names::NTuple, arrays...)
         return new{_construct(arrays)..., names}(arrays)
     end
@@ -79,5 +98,13 @@ end
 
 const NamedTupleVector{Ttup, Nt} = NamedTupleArray{Ttup, 1, Nt}
 const NamedTupleMatrix{Ttup, Nt} = NamedTupleArray{Ttup, 2, Nt}
+
+NamedTupleVector(args...) = NamedTupleArray(args...)
+NamedTupleMatrix(args...) = NamedTupleMatrix(args...)
+
+for _type in (:NamedTupleArray, :TupleArray, :AbstractTupleArray, :AbstractNamedTupleArray,
+              :NamedTupleVector, :TupleVector)
+    @eval _striptype(::Type{<:$_type}) = $_type
+end
 
 end # module TupleArrays
