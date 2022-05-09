1b488367465a170cd4f20e548992b38f4b25c440
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-05-08 21:43:16 -0400

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,25 @@ using TupleArrays
 using Test
 
 @testset "TupleArrays.jl" begin
-    let ta = TupleArray(1:10, 11:20)
-        @test isa(ta, TupleVector)
-        @test length(ta) == 10
-        @test tuplength(ta) == 2
+    tup = (1:10, 11:20)
+    for data = (tup, collect.(tup))
+        let ta = TupleArray(data...)
+            @test isa(ta, TupleVector)
+            @test length(ta) == 10
+            @test tuplength(ta) == 2
+            @test sum(prod, ta) == 935
+            @test firstindex(ta) == 1
+            @test lastindex(ta) == 10
+        end
     end
-
-    let ta1 = TupleArray(1:10, 11:20, 21:30)
-        @test isa(ta1, TupleVector)
-        @test length(ta1) == 10
-        @test tuplength(ta1) == 3
+    tup = (1:10, 11:20, 21:30)
+    for data = (tup, collect.(tup))
+        let ta1 = TupleArray(data...)
+            @test isa(ta1, TupleVector)
+            @test length(ta1) == 10
+            @test tuplength(ta1) == 3
+            @test firstindex(ta1) == 1
+            @test lastindex(ta1) == 10
+        end
     end
 end
